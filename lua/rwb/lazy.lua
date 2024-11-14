060b62405127da9b27f0fd062e3bcad8246d04f0
Robert W. Baskette <rwbaskette@gmail.com>
2024-11-13 20:21:54 -0500

    chore: disables experimental lsp-indent
    LSP Formats files as well

--- lua/rwb/lazy.lua
+++ lua/rwb/lazy.lua
@@ -5,7 +5,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
       { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
+      { out,                            "WarningMsg" },
       { "\nPress any key to exit..." },
     }, true, {})
     vim.fn.getchar()
@@ -15,84 +15,90 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-  --[[ Language ]]--
-  { "nvim-treesitter/nvim-treesitter",
+  --[[ Language ]] --
+  {
+    "nvim-treesitter/nvim-treesitter",
     compilers = { "cl" },
     build = ":TSUpdate",
-    config = function () 
+    config = function()
       local configs = require("nvim-treesitter.configs")
       configs.setup({
         ensure_installed = { "c", "go", "vim", "vimdoc", "javascript", "html" },
         sync_install = false,
         highlight = { enable = false },
-        indent = { enable = true },  
+        indent = { enable = false },
       })
     end
   },
-  {'VonHeikemen/lsp-zero.nvim', branch = 'v4.x'},
-  {'williamboman/mason.nvim'},
-  {'williamboman/mason-lspconfig.nvim'},
-  {'neovim/nvim-lspconfig'},
-  {'hrsh7th/cmp-nvim-lsp'},
-  {'hrsh7th/nvim-cmp'},
+  { 'VonHeikemen/lsp-zero.nvim',        branch = 'v4.x' },
+  { 'williamboman/mason.nvim' },
+  { 'williamboman/mason-lspconfig.nvim' },
+  { 'neovim/nvim-lspconfig' },
+  { 'hrsh7th/cmp-nvim-lsp' },
+  { 'hrsh7th/nvim-cmp' },
 
-  --[[ Function ]]--
-  { "nvim-telescope/telescope.nvim", opts = {
-    pickers = {
-      buffers = {
-          sort_lastused=1, 
-          ignore_current_buffer=1
+  --[[ Function ]] --
+  {
+    "nvim-telescope/telescope.nvim",
+    opts = {
+      pickers = {
+        buffers = {
+          sort_lastused = 1,
+          ignore_current_buffer = 1
+        }
       }
     }
-  } },
-  { "numToStr/Comment.nvim", opts = {
-    ---Add a space b/w comment and the line
-    padding = true,
-    ---Whether the cursor should stay at its position
-    sticky = true,
-    ---Lines to be ignored while (un)comment
-    ignore = nil,
-    ---LHS of toggle mappings in NORMAL mode
-    toggler = {
-      -- NerdComment-ify
-      ---Line-comment toggle keymap
-      line = 'gcc',
-      ---Block-comment toggle keymap
-      block = 'gcb',
-    },
-    ---LHS of operator-pending mappings in NORMAL and VISUAL mode
-    opleader = {
-      ---Line-comment keymap
-      line = 'gc',
-      ---Block-comment keymap
-      block = 'gb',
-    },
-    ---LHS of extra mappings
-    extra = {
-      ---Add comment on the line above
-      above = 'gcO',
-      ---Add comment on the line below
-      below = 'gco',
-      ---Add comment at the end of line
-      eol = 'gcA',
-    },
-    ---Enable keybindings
-    ---NOTE: If given `false` then the plugin won't create any mappings
-    mappings = {
-      ---Operator-pending mapping; `gcc` `gbc` `gc[count]{motion}` `gb[count]{motion}`
-      basic = true,
-      ---Extra mapping; `gco`, `gcO`, `gcA`
-      extra = true,
-    },
-    ---Function to call before (un)comment
-    pre_hook = nil,
-    ---Function to call after (un)comment
-    post_hook = nil,
-  } },
+  },
+  {
+    "numToStr/Comment.nvim",
+    opts = {
+      ---Add a space b/w comment and the line
+      padding = true,
+      ---Whether the cursor should stay at its position
+      sticky = true,
+      ---Lines to be ignored while (un)comment
+      ignore = nil,
+      ---LHS of toggle mappings in NORMAL mode
+      toggler = {
+        -- NerdComment-ify
+        ---Line-comment toggle keymap
+        line = 'gcc',
+        ---Block-comment toggle keymap
+        block = 'gcb',
+      },
+      ---LHS of operator-pending mappings in NORMAL and VISUAL mode
+      opleader = {
+        ---Line-comment keymap
+        line = 'gc',
+        ---Block-comment keymap
+        block = 'gb',
+      },
+      ---LHS of extra mappings
+      extra = {
+        ---Add comment on the line above
+        above = 'gcO',
+        ---Add comment on the line below
+        below = 'gco',
+        ---Add comment at the end of line
+        eol = 'gcA',
+      },
+      ---Enable keybindings
+      ---NOTE: If given `false` then the plugin won't create any mappings
+      mappings = {
+        ---Operator-pending mapping; `gcc` `gbc` `gc[count]{motion}` `gb[count]{motion}`
+        basic = true,
+        ---Extra mapping; `gco`, `gcO`, `gcA`
+        extra = true,
+      },
+      ---Function to call before (un)comment
+      pre_hook = nil,
+      ---Function to call after (un)comment
+      post_hook = nil,
+    }
+  },
 
-  --[[ Appearance ]]--
+  --[[ Appearance ]] --
   { "letorbi/vim-colors-modern-borland", config = function() end },
-  { "chriskempson/base16-vim", config = function() end },
-  { "nvim-lualine/lualine.nvim", opts = { dependencies = { "nvim-tree/nvim-web-devicons" }, theme = "auto" } }
+  { "chriskempson/base16-vim",           config = function() end },
+  { "nvim-lualine/lualine.nvim",         opts = { dependencies = { "nvim-tree/nvim-web-devicons" }, theme = "auto" } }
 })
-
